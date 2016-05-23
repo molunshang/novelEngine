54a045e6a58bf1c50a413027489ee9301315388b
molunshang <molunshang@163.com>
2016-05-24 00:56:14 +0800

    添加服务端resetful接口

--- novelSpider/novelSpider.py
+++ novelSpider/novelSpider.py
@@ -1,15 +1,46 @@
-from flask import Flask
+from flask import Flask;
+from flask import jsonify;
 from pymongo import mongo_client;
 
 app = Flask(__name__)
-client = mongo_client.MongoClient(host='127.0.0.1', port=27017)
-db = client["book"];
+client = mongo_client.MongoClient(host='127.0.0.1', port=27017);
+bookDb = client["book"];
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/search/<name>')
+def search(name):
+    cursor = bookDb.Book.find({"BookName": name});
+    return jsonify(isok=True, data=toArray(cursor));
+
+
+@app.route('/list/<int:index>')
+def list(index):
+    index = int(index) - 1;
+    if index < 0:
+        index = 0;
+    cursor = bookDb.Book.find().skip(30 * index).limit(30);
+    return jsonify(isok=True, data=toArray(cursor));
+
+
+@app.route('/detail/<int:bookId>')
+def detail(bookId):
+    bookId = int(bookId);
+    if bookId < 1:
+        return jsonify(isok=False);
+    cursor = bookDb.BookLinks.find({"BookId": bookId});
+    return jsonify(isok=True, data=toArray(cursor));
+
+
+def toArray(cursor):
+    result = [];
+    try:
+        with cursor as c:
+            for r in c:
+                result.append(r);
+    except Exception as e:
+        print(e);
+    return result;
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='192.168.1.78')
